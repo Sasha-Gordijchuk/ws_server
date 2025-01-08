0033676663f9f9e895aebfec1ebbddc066d21132
Olexandr Gordijchuk <san4ez1717@gmail.com>
2025-01-08 10:37:08 +0200

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.send(JSON.stringify({ type: "info", message: "Connected to signaling server" }));
+});
+
+console.log("WebSocket server is running on ws://localhost:8080");

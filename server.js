c72c2218613af177e750fef9922bfa4a7b174583
Olexandr Gordijchuk <san4ez1717@gmail.com>
2025-01-10 16:40:59 +0200

    server uptaed

--- server.js
+++ server.js
@@ -1,17 +1,120 @@
-const WebSocket = require("ws");
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const {version, validate} = require('uuid');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const ACTIONS = {
+  JOIN: 'join',
+  LEAVE: 'leave',
+  SHARE_ROOMS: 'share-rooms',
+  ADD_PEER: 'add-peer',
+  REMOVE_PEER: 'remove-peer',
+  RELAY_SDP: 'relay-sdp',
+  RELAY_ICE: 'relay-ice',
+  ICE_CANDIDATE: 'ice-candidate',
+  SESSION_DESCRIPTION: 'session-description'
+};
+const PORT = process.env.PORT || 8080;
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+function getClientRooms() {
+  const {rooms} = io.sockets.adapter;
+
+  return Array.from(rooms.keys()).filter(roomID => validate(roomID) && version(roomID) === 4);
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms()
+  })
+}
+
+io.on('connection', socket => {
+  console.log('connection');
+  
+  shareRoomsInfo();
+
+  socket.on(ACTIONS.JOIN, config => {
+    const {room: roomID} = config;
+    const {rooms: joinedRooms} = socket;
+
+    if (Array.from(joinedRooms).includes(roomID)) {
+      return console.warn(`Already joined to ${roomID}`);
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false
+      });
+
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      });
+    });
+
+    socket.join(roomID);
+    shareRoomsInfo();
+  });
+
+  function leaveRoom() {
+    const {rooms} = socket;
+
+    Array.from(rooms)
+      // LEAVE ONLY CLIENT CREATED ROOM
+      .filter(roomID => validate(roomID) && version(roomID) === 4)
+      .forEach(roomID => {
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients
+          .forEach(clientID => {
+          io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+            peerID: socket.id,
+          });
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          });
+        });
+
+        socket.leave(roomID);
+      });
+
+    shareRoomsInfo();
+  }
+
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+  socket.on('disconnecting', leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({peerID, sessionDescription}) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
     });
   });
 
-  ws.send(JSON.stringify({ type: "info", message: "Connected to signaling server" }));
+  socket.on(ACTIONS.RELAY_ICE, ({peerID, iceCandidate}) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+
 });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+server.listen(PORT, () => {
+  console.log('Server Started on port: ' + PORT)
+})
